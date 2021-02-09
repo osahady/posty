e2b52ce26e10f667b922758c6eecceb8bff74d29
Osama Abdelhady <osahady@gmail.com>
2021-02-09 22:15:27 +0300

    main route /

--- routes/web.php
+++ routes/web.php
@@ -9,6 +9,9 @@ use App\Http\Controllers\PostLikeController;
 use App\Http\Controllers\UserPostController;
 use Illuminate\Support\Facades\Route;
 
+Route::get('/', function(){
+   return view('home');
+});
 
 Route::get('/home', function () {
    return view('home');
